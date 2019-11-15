3159b20c2b04e29e5e1d317098f85b39c2bfa321
Sukhvinder Singh <karramsos@gmail.com>
2019-11-15 11:19:30 +0100

    Update twitter1.py

--- Twitter/twitter1.py
+++ Twitter/twitter1.py
@@ -10,6 +10,5 @@ access_secret = os.environ['TWITTER_ACCESS_SECRET']
 
 
 t = Twitter(auth=OAuth(access_token, access_secret, consumer_key, consumer_secret))
-#t = Twitter(auth=OAuth('22378452-EnmlHrzpURC91d36Huslll575Ayn8Cqse6jTHn4Sn', 'IilDkC8qJUA8a6tiocZPJL4MIQHiXxHnX6cg3XpcUOjwk', 'H9dBp4KxXXXhhL5kzn0yYXvqO', 'imKqXnxkoiEnRRsi7WNwgKxbl5tKt23la7syePpQ6frp4mREYP'))
 pythonTweets = t.search.tweets(q="#python")
 print(pythonTweets)
