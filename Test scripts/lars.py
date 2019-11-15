0a4f8c999f90a6345d07a28f94f09296af677d34
Sukhvinder Singh <karramsos@gmail.com>
2019-11-15 11:13:48 +0100

    Delete lars.py

--- Test scripts/lars.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-html = urlopen("http://static.norges-bank.no/contentassets/b36f0051784546c5a56ce612036e9c4c/ppr_2_2017_tall.htm?v=06/22/2017140842&ft=.htm")
-bsObj = BeautifulSoup(html.read(), 'lxml')
-print(bsObj.prettify)
-
-
-<a href="http://static.norges-bank.no/contentassets/b36f0051784546c5a56ce612036e9c4c/ppr_2_2017_tall.xlsx?v=06/22/2017140827&amp;ft=.xlsx">Tallsett</a>
